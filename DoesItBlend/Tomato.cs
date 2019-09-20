f98eca25a951a65ac813dbc5433d4e86af181b8a
jmporter1998 <jmporter@ksu.edu>
2019-09-20 16:09:16 -0500

    commit

---
+++ DoesItBlend/Tomato.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DoesItBlend
+{
+    class Tomato : Fruit
+    {
+        public override string Blend()
+        {
+            return "Red mush, juice, and seeds";
+        }
+    }
+}
